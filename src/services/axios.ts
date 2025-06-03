d54e7a8de2ba0b320dfb2497ff6c14c7d9e9fadf
valentim <valentimdesenvolvedor@gmail.com>
2025-06-03 17:22:51 -0300

    final build

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://54.198.191.101', // Altere para a URL da API se subir para produção
+  baseURL: '//54.198.191.101', // Altere para a URL da API se subir para produção
 });
 
 // Lista de endpoints públicos (que não exigem token)
